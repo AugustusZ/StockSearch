2615b8406c77214395bf579b50fe8a0e20f9039d
AugustusZ <augustus.z@outlook.com>
2016-04-05 15:17:03 -0700

    + News Feeds

--- main.php
+++ main.php
@@ -8,6 +8,26 @@
         echo file_get_contents($lookup_api_url.urlencode($_GET["input"]));
     } else if (isset($_GET["symbol"])) {
     	echo file_get_contents($quote_api_url.urlencode($_GET["symbol"]));
+    } else if (isset($_GET['query'])) {
+    	echo getBingSearchResult($_GET['query']);
     }
 
+?>
+
+<?php            
+   function getBingSearchResult($query) {
+       $accountKey = 'pWZPGiXLvJZyIdRVmXoSVrSaVrXA38UOP4idctL9xhM';
+       $ServiceRootURL = 'https://api.datamarket.azure.com/Bing/Search/v1/';
+       $WebSearchURL = $ServiceRootURL . '/News?$format=json&Query=';
+       
+       $context = stream_context_create(array(
+           'http' => array(
+               'request_fulluri' => true,
+               'header'  => "Authorization: Basic " . base64_encode($accountKey . ":" . $accountKey)
+           )
+       ));
+
+       $request = $WebSearchURL . urlencode( '\'' . $query . '\'');
+       return file_get_contents($request, 0, $context);
+   }
 ?>
